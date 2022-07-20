c99927ee141ca35b58c831ac579c270bd13da4ba
Feelminho <minhoyamza007@gmail.com>
2022-07-20 09:37:45 +0400

    0.3add

--- addTwoInt.py
+++ addTwoInt.py
@@ -1,16 +1,22 @@
-
 #!/bin/python3
 
 import sys
 
-def add(x,y):
-return x+y
+def add(a,b):
+	return(a+b)
 
-nbr_arg=len (sys.argv)-1
-if (nbr_arg < 2):
-print ("Error")
 
-x=int(sys.argv[1])
-y=int(sys.argv[2])
+def main():
+	arg=len(sys.argv)-1
+	if (arg < 2):
+		print("Error occured")
+		x = int(sys.argv[1])
+		y = int(input("Enter another values"))
+		print (add(x,y))
 
-print (add(x,y))
+	else:
+		x= int(sys.argv[1])
+		y= int(sys.argv[2])
+		print (add(x,y))
+
+main()
